fbdafa80225ab63b3724a7cca1d9198d8a3faf29
Heber <parzival.html@gmail.com>
2023-02-23 14:25:24 +0100

    Dibujo de matriz, movimiento de pieza en descenso en intervalo fijo, función para fusionar posición actual del tetromino en el tablero

--- tetris.js
+++ tetris.js
@@ -13,6 +13,21 @@ const matrix = [
 
 ];
 
+// Función para crear matriz
+function createMatrix(width, height) {
+    
+    const matrix = [];
+
+    // Mintras la altura especificada por parámetro no sea 0,
+    // añade un nuevo array de longitud especificada por parámetro
+    // con todos los valores por defecto a 0
+    while(height != 0){
+        matrix.push(new Array(width).fill(0))
+
+        height--
+    }
+}
+
 // Función de render general
 function draw(){
 
@@ -21,7 +36,6 @@ function draw(){
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     drawMatrix(player.matrix, player.pos);
-
 }
 
 // Offset :Object {x: int, y: int} agregar valores de posición a las piezas
@@ -42,17 +56,82 @@ function drawMatrix(matrix, offset){
 
 };
 
-// Función encargada de actualizar el estado del tablero
-function update(){
-    draw();
-    requestAnimationFrame(update);
+// Función para copiar los valores del tetromino en posesión del jugador en el tablero en su posición específica 
+function merge(arena, player){
+
+    // Iterar cada fila de la matriz del jugador
+    player.matrix.forEach((row, y) => {
+
+        // Iterar cada valor de la fila del jugador
+        row.forEach((value, x) => {
+
+            // Si el valor de la estructura del tetromino es 0, no se renderiza, sino
+            // se copia los valores del tetromino en su posición actual al tablero
+            if(value !== 0){
+
+                arena[y + player.pos.y][x + player.pos.x] = value;
+            }
+        })
+    })
 }
 
-// Objeto player 
+// Desciende la posición de la pieza en 1 manualmente haciendo reset del tiempo de ciclo a 0
+function playerDrop(){
+    player.pos.y++;
+    dropCounter = 0;
+}
+
+// Función encargada de actualizar el estado del tablero
+
+let dropCounter = 0; // Acumula la diferencia de tiempo en cada ciclo
+let dropInterval = 1000;
+let lastTime = 0; // Último registro en tiempo de ciclo anterior
+
+function update(time = 0){ // time almacena el tiempo total transcurrido desde la ejecución del programa
+
+    const diffTime = time - lastTime;
+    lastTime = time;
+
+    dropCounter += diffTime;
+    if(dropCounter > dropInterval){
+
+        // Movimiento vertical descendente cada 1s
+        playerDrop();
+    }
+
+    draw();
+
+    // Informa al navegador de un animación en curso, solicitando un render nuevo de la página.
+    // Se debe invocar dentro de la misma función callback que recibe por parámetro para que sea recursivo.
+    requestAnimationFrame(update);
+
+}
+
+// Creación de tablero/matriz del juego
+const arena = createMatrix(12, 20);
+
+// console.log(arena)
+// console.table(arena)
+
+// Objeto player (tetromino/matriz en posesión del jugador)
 const player = {
-    pos: {x: 5, y: 5},
+    pos: {x: 5, y: 0},
     matrix: matrix,
 }
 
-update();
+document.addEventListener('keydown', event => {
 
+    console.log(event);
+    //
+    if(event.keyCode === 37){
+        player.pos.x--;
+    }
+    else if(event.keyCode === 39){
+        player.pos.x++;
+    }
+    else if(event.keyCode === 40){
+        playerDrop();
+    }
+});
+
+update();
